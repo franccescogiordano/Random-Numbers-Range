a5906d9f6cca4375bdb5ef14f53a5a52b992fd5a
francheskito <franccesco.giordano11@gmail.com>
2022-03-25 19:36:04 -0300

    funcionalidad terminada

--- front/src/components/Result.jsx
+++ front/src/components/Result.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux';
 const Result = (props) => {
   let contador = 0;
 
-  let numeritos = props.result?.todoarmado?.map(function (num) {
-    contador++;
-    if (contador % props.result?.columns == 0) {
+  
+/*for (let value of values) {
+  contents.push(<span key={value.key}>{value.name}</span>);
 
-
-      return  (<span><br/>{num}</span>);
-    }
-    return  <span>{num}</span>;
+  if (value.condition) {
+    contents.push(<br key={`${value.key}-br`} />);
   }
+})*/
+const contents = props.result?.todoarmado?.map((value,contador) => {
 
-  )
-    .join()
+  if (contador % props.result?.columns == 0)
+    return (<><br key={contador+"-br"}/>
+      <span key={contador}>{value},</span></>);
+  return (<span key={contador}>{value},</span>)
+})
+
   console.log("props!!!!!!!!:" + props)
   return <div>
     Resultado:
- {numeritos}
+ {contents}
 
   </div>
 }
